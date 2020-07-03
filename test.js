aaf5d555bc6bb2a09639470a0ef9056056aadc20
hichamdandan23 <hdandan@miu.edu>
2020-07-03 09:26:43 -0500

    inheritance

---
+++ test.js
@@ -0,0 +1,17 @@
+function longestNoSpace(s, ...more)
+{
+  let maxSpace = 0;
+
+  
+
+}
+
+function countSpace(s)
+{
+    let countSpace = 0;
+    for(let i = 0; i < s.length ; i++)
+        if(s[i] === ' ') countSpace++;
+        
+        
+    return countSpace;
+}
